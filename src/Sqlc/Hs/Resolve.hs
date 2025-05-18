ef35689f26cae68ecbeaecca3c781215d98c1050
Alex Biehl <alex@scarf.sh>
2025-05-18 20:10:29 +0200

    Get nullability right

--- src/Sqlc/Hs/Resolve.hs
+++ src/Sqlc/Hs/Resolve.hs
@@ -270,20 +270,12 @@ overrideToMatcher override =
 
     -- TODO extend the matching to support overriding individual columns
     matchType column
-      | not $
-          matchesNullability override.nullable (not (column ^. #notNull)) =
-          -- Do not match on a nullable column if the override doesn't allow it
+      | fromMaybe False override.nullable /= not (column ^. #notNull) =
           Nothing
       | columnDataType (column ^. #type') == override.databaseType =
           Just (pure override.haskellType)
       | otherwise =
           Nothing
-
-    matchesNullability Nothing _nullable = True
-    matchesNullability (Just True) True = True
-    matchesNullability (Just False) False = True
-    matchesNullability (Just True) False = False
-    matchesNullability (Just False) True = False
 
 enumMatcher ::
   -- | HaskellType pointing to the types module.
